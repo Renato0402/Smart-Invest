e890d07621e78244609aad1b0637df215605550f
Paulo Ubirajara <ubirajara3@hotmail.com>
2020-11-04 12:25:16 -0300

    Adicionar mCRUD básico para investimento

--- src/main/java/br/unifor/smartinvest/controller/InvestimentoController.java
+++ src/main/java/br/unifor/smartinvest/controller/InvestimentoController.java
@@ -1,11 +1,12 @@
 package br.unifor.smartinvest.controller;
 
-import br.unifor.smartinvest.model.Cliente;
-import br.unifor.smartinvest.model.Corretora;
 import br.unifor.smartinvest.model.Investimento;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
+@RestController
+@RequestMapping("/investimentos")
 public class InvestimentoController {
 	private ArrayList<Investimento> listaInvestimentos;
 
@@ -13,15 +14,73 @@ public class InvestimentoController {
 		this.listaInvestimentos = new ArrayList<>();
 	}
 
-	public Cliente verCliente(int clienteId) {
-		// Retornar cliente pelo banco de dados
+	@GetMapping
+	public ArrayList<Investimento> get() {
+		return listaInvestimentos;
+	}
+
+	@GetMapping("/{id}")
+	public Investimento get(@PathVariable("id") int id) {
+		Investimento investimento = buscarInvestimentoPorClienteId(id);
+		if(investimento != null)
+			return investimento;
+
 		return null;
 	}
 
-	public Corretora verCorretora(int corretoraId) {
-		// Retornar corretora pelo banco de dados
+	@PostMapping
+	public String post(@RequestBody Investimento investimento) {
+		if(investimento != null) {
+			this.listaInvestimentos.add(investimento);
+			return "Investimento realizado com sucesso!";
+		}
+
+		return "Erro ao investir!";
+	}
+
+	@PutMapping
+	public String put(@RequestHeader int id, @RequestBody Investimento investimento) {
+		if(alterarClientePorId(id, investimento))
+			return "Alteração de investimento realizada com sucesso!";
+
+		return "Não foi encontrado um investimento realizado por este cliente!";
+	}
+
+	@DeleteMapping
+	public String delete(@RequestHeader int id) {
+		if(removerClientePorId(id))
+			return "Remoção de investimento realizada com sucesso!";
+
+		return "Não foi encontrado um investimento realizado por este cliente!";
+	}
+
+	private Investimento buscarInvestimentoPorClienteId(int id) {
+		for (Investimento listaInvestimento : listaInvestimentos)
+			if (listaInvestimento.getCliente().getClienteId() == id)
+				return listaInvestimento;
+
+
 		return null;
 	}
 
-	// Métodos para adicionar à lista de investimentos //
+	private boolean alterarClientePorId(int id, Investimento novoInvestimento) {
+		Investimento antigoInvestimento = this.buscarInvestimentoPorClienteId(id);
+		if(antigoInvestimento != null && novoInvestimento != null) {
+			int posAntigoInvestimento = listaInvestimentos.indexOf(antigoInvestimento);
+			listaInvestimentos.set(posAntigoInvestimento, novoInvestimento);
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean removerClientePorId(int id) {
+		Investimento investimento = this.buscarInvestimentoPorClienteId(id);
+		if(investimento != null) {
+			listaInvestimentos.remove(investimento);
+			return true;
+		}
+
+		return false;
+	}
 }
